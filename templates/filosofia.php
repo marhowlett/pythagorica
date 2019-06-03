72e7e934b5fe502d763173854adfe3255bc55f0f
Maria del Mar Ramonell <marimar_r2003@hotmail.com>
2019-06-03 16:12:48 -0500

    ,

---
+++ templates/filosofia.php
@@ -0,0 +1,83 @@
+<?php
+    /* Template Name: Filosofia */ 
+    get_header();
+?>
+   
+    
+  
+    <section id="banner" class="template-section">
+       <h1>Filosofía Pythagorica </h1>
+    </section >
+     <section id="escuela" class="template-section">
+         <div class="contenedor">
+         <p class="finalidad">La finalidad de la <strong> Academia de Filosofía Pythagorica</strong> de la actualidad, nuestro siglo XXI, es reeditar la Escuela Pythagorica de Divina Sabiduría de cinco siglos A. C. en donde estaba, nada menos que la gigantesca figura, misteriosa y enigmática del Maestro Pythagoras, de quien se decía era…
+<strong>filósofo, matemático, músico, médico, astrólogo, biólogo, físico, metafísico, y Maestro de Sabiduría Divina….</strong> </p>
+        <p>Era una escuela vastísima, se dice que es la primera universidad del mundo occidental, que llegó a tener durante décadas 3000 alumnos permanentes, en pabellones de varones y pabellones de mujeres (en la época que no era común que la mujer reciba con el hombre, un trato completamente igualitario); enseñando: qué es el mundo conocido, el porqué de sus particularidades, de qué mundo desconocido provenimos; cuáles son nuestros cuerpos, sus formas de manejo y curación, etc. 
+</p>
+        <p>Es El Bien lo que todos buscan consciente o inconscientemente, aun los que roban y matan, no quieren que se los agreda ni que se los robe; por consiguiente:
+</p>
+        <h2>Para Pythagoras, el bien no es un concepto cultural sino una energía completa y perfecta que no está en la cabeza, y requiere su búsqueda diaria.</h2>
+         </div>
+</section>
+     <section id="plasmar" class="template-section">   
+           <div class="contenedor">
+            <div class="grid">
+
+                <div class="col">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/academia.jpg" alt="Logo">
+        
+                    
+
+                </div>
+                <div class="col prueba_col">
+                   <ul>
+                       <li>La Academia enseña cómo plasmar el Bien, y dice que, en las diferentes culturas del mundo a Dios, se le llama Bien.</li>
+                       <li>La Academia enseña a buscar y encontrar la propia Identidad…conocer y reconocer la propia Alma Inmortal…</li>
+                       <li>Con la finalidad que el ser humano vibre en su número y en su música y aprenda a no ser una respuesta al afuera.</li>
+                       
+                   </ul>
+                </div>
+
+
+            </div>
+         </div>
+</section>
+      <section id="decia" class="template-section">
+         <div class="contenedor">
+         <p>Para <strong>Pythagoras</strong> el propósito esencial de la Academia es enseñar al ser humano a plasmar la cultura del Bien en la Tierra para conseguir su propia felicidad y la del mundo.  </p>
+         <p class="de">Decía que: </p>
+         <h2>“El Propósito de la Escuela es elevar a la raza humana y sus cerebros a que entiendan y luego busquen los valores más sublimes del Espíritu”</h2>
+         <p>Cada uno de acuerdo a su nivel evolutivo. Que el humano encarnado entienda su Alma, entienda su Espíritu y por fin entienda el maravilloso Poder que puede manejar, si permite que su Espíritu lo ilumine y si entiende y maneja su Alma. Con ello logrará traslucirlo por todos los poros de su piel.
+ <strong>Este “Poder” es el que hace evolucionar, no el poder humano… el Poder trascendental, el Poder espiritual….</strong></p>
+          </div>
+</section>
+  <section id="principios" class="template-section">
+         <div class="contenedor">
+         <h2>Principios  Fundamentales</h2>
+         <p>Los principios fundamentales de esta filosofía están representados y explicados por las leyes que rigen todo lo creado. <strong> Como entender la simpleza de que el fuego quema y el agua moja… </strong></p>
+         <p>Así de simple, devela Pythagoras las Leyes que explican todo lo creado y la propia existencia de cada ser humano, como ser individual y único... así uno aprende a entenderse a sí mismo, en su parte inmortal y en su parte mortal hasta en detalle; como ser la herencia genética de su encarnación actual, su familia de origen, su raza. Se aprende a explicar el por qué y para qué del mundo que nos rodea. </p>
+         <p class="de">Y con ello el encarnado devela por fin “qué es” la Misteriosa Vida…</p>
+      </div>
+</section>
+ 
+ <section id="aprende" class="template-section">
+         <div class="contenedor">
+         <h2>Qué se aprende y enseña</h2>
+         <p>En la Escuela Pythagorica aprendemos el manejo de lo Mortal y el manejo de lo I­nmortal. Si no despertamos a la real Conciencia de lo Inmortal no podremos manejar lo Mortal. </p>
+         <p>Es decir, lo que es fuente y raíz de nuestras aflicciones: los deseos… o lo que es lo mismo, nuestros gustos, necesidades, requerimientos, apetencias; nuestro carácter; el manejo de los cinco sentidos; el manejo de las inestables emociones, que para Pythagoras son cerebrales (hemisferio cerebral derecho), y nada tienen que ver con los sentimientos profundos y estables que están en el Alma. </p>
+         <p>Es vital que el ser humano se reconozca a sí mismo través del fluido de su propia Alma atravesando su cerebro y su cuerpo. Que la sienta, que la perciba en sus células en sus moléculas… Que pueda sentir a su Alma vibrando en sus manos, vibrando en su voz, vibrando en su forma de mirar.</p>
+         <p> Es vital entender que somos seres inmortales viajando en el mundo mortal. Nos metemos en este envase carnal y sólo lo Inmortal tiene manejo sobre lo Mortal. Por eso es que Pythagoras señala como Principio básico a sus discípulos: “nadie tiene poder sobre ti, sólo Dios y tu parte Inmortal, no te dejes manejar por nadie, ni por nada” …</p>
+      </div>
+</section>
+   <section id="despertar" class="template-section">
+         <div class="contenedor">
+         <p>Por eso la Filosofía Pythagorica quiere <strong>despertar al Maestro Interior….</strong> y sostiene “no hay Maestros en esta tierra” salvo el Cristo, o seres muy superiores que cuando oran vencen la Ley de la Gravedad, el resto somos madero horizontal de la cruz. </p>
+         <p>La <strong>Escuela Pythagorica</strong> quiere generar seres completamente libres. Por eso es que el despertar el propio Maestro Interior, nos hace despertar con verdad y humildad la propia psiquis y visualizar el Camino de transformación para que despertando virtudes del Alma se lo pueda transitar. Se entiende que el Poder de plasmar la auténtica Felicidad, se basa en el adiestramiento diario de nuestra voluntad para que se oriente al servicio del Bien y no al servicio de nuestra propia psiquis, sus deseos, banalidades y vanidades….</p>
+       </div>
+</section>
+   
+  <?php include 'contacto.php'; ?>
+ 
+<?php 
+    get_footer();
+?>
